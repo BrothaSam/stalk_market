53d50f78c71eb5cc3d2615da6eaec5554b170b70
Sam <samemorgan@gmail.com>
2020-04-23 20:52:25 -0500

    save-price

---
+++ commands/save-price.js
@@ -0,0 +1,33 @@
+const { prefix, days, meridiem } = require('../config.json');
+
+module.exports = {
+  name: 'save-price',
+  description:
+    'Saves the price of a single turnip to the database. User can provide optional day and meridiem.',
+  args: true,
+  usage: '<price> [<day> <am/pm>]',
+  execute(message, args) {
+    const price = args[0];
+
+    if (args.length === 1 && !isNaN(args[0])) {
+      const price = args[0];
+      message.reply(`your turnip price of ${price} bells has been recorded!`);
+    } else if (args.length === 3) {
+      if (days.includes(args[1]) && meridiem.includes(args[2])) {
+        const day = args[1].charAt(0).toUpperCase() + args[1].slice(1);
+        const meridiem = args[2].toUpperCase();
+        message.reply(
+          `your turnip price of ${price} bells, for ${day} ${meridiem} has been recorded!`
+        );
+      } else {
+        message.reply(
+          `you did not provide the proper arguments.\nCorrect usage is "${prefix}${this.name} ${this.usage}".`
+        );
+      }
+    } else {
+      message.reply(
+        `you did not provide the proper arguments.\nCorrect usage is "${prefix}${this.name} ${this.usage}".`
+      );
+    }
+  },
+};
--- index.js
+++ index.js
@@ -1,17 +1,50 @@
-const Discord = require("discord.js");
-const { prefix, token } = require("./config.json");
+const Discord = require('discord.js');
+const fs = require('fs');
+const { prefix } = require('./config.json');
+require('dotenv').config();
 
 const client = new Discord.Client();
 
-client.once("ready", () => {
+client.commands = new Discord.Collection();
+
+const commandFiles = fs
+  .readdirSync('./commands')
+  .filter((file) => file.endsWith('.js'));
+
+client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.on("message", (message) => {
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+
+  client.commands.set(command.name, command);
+}
+
+client.on('message', (message) => {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  const args = message.content.slice(prefix.length).split(" ");
-  const command = args.shift().toLowerCase();
+  const args = message.content.slice(prefix.length).toLowerCase().split(/ +/);
+  const commandName = args.shift().toLowerCase();
+
+  if (!client.commands.has(commandName)) return;
+
+  const command = client.commands.get(commandName);
+
+  console.log(message.content);
+
+  if (command.args && !args.length) {
+    return message.reply(
+      `you didn't provide any of the required arguments!\nCorrect usage is "${prefix}${command.name} ${command.usage}".`
+    );
+  }
+
+  try {
+    command.execute(message, args);
+  } catch (err) {
+    console.error(err);
+    message.reply(`There was an error trying to execute "${message.content}"`);
+  }
 });
 
-client.login(token);
+client.login(process.env.TOKEN);
