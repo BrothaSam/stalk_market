77422735057c30a96c5f21b58ac05dc18a922e20
Sam <samemorgan@gmail.com>
2020-04-23 22:09:51 -0500

    meridiem to period

--- commands/save-price.js
+++ commands/save-price.js
@@ -1,10 +1,10 @@
-const { days, meridiem } = require('../config.json');
+const { days, period } = require('../config.json');
 const { improperArguments } = require('../default-responses');
 
 module.exports = {
   name: 'save-price',
   description:
-    'Saves the price of a single turnip to the database. User can provide optional day and meridiem.',
+    'Saves the price of a single turnip to the database. User can provide optional day and period.',
   args: true,
   usage: '<price> [<day> <am/pm>]',
   execute(message, args) {
@@ -14,11 +14,11 @@ module.exports = {
       const price = args[0];
       message.reply(`your turnip price of ${price} bells has been recorded!`);
     } else if (args.length === 3) {
-      if (days.includes(args[1]) && meridiem.includes(args[2])) {
+      if (days.includes(args[1]) && period.includes(args[2])) {
         const day = args[1].charAt(0).toUpperCase() + args[1].slice(1);
-        const meridiem = args[2].toUpperCase();
+        const period = args[2].toUpperCase();
         message.reply(
-          `your turnip price of ${price} bells, for ${day} ${meridiem} has been recorded!`
+          `your turnip price of ${price} bells, for ${day} ${period} has been recorded!`
         );
       } else {
         message.reply(improperArguments(this.name, this.usage));
